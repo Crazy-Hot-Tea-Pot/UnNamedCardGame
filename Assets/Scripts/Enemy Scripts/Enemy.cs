579f747401245cd2d52becdb0746b4cb3b396cd6
Sebastian <sebander01@gmail.com>
2024-10-08 13:20:37 -0400

    Changedprivateupdatetopublicupdate

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -213,7 +213,7 @@ public class Enemy : MonoBehaviour
         UIEnemyMaxHealthStart();
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
         //Update UI for enemy health
         UIEnemyHealth();
