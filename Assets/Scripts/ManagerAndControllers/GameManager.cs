6f8e88c03f4647dc363c254de5e4a6e9aa3db010
Hammer1624 <98486449+Hammer1624@users.noreply.github.com>
2024-11-08 14:55:42 -0500

    AddedBackgroundnoise

--- Assets/Scripts/ManagerAndControllers/GameManager.cs
+++ Assets/Scripts/ManagerAndControllers/GameManager.cs
@@ -109,7 +109,7 @@ public class GameManager : MonoBehaviour
         //DrawChip(DrawsPerTurn);
 
         //test
-       // SoundManager.StartBackground(SoundManager.BgSound.Test);
+       SoundManager.StartBackground(BgSound.Background);
 
     }
 
