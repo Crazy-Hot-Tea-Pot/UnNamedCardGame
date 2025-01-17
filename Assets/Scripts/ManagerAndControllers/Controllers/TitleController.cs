5937e1c2dcb45ec139c0eb253f8b74e736017a14
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2025-01-17 13:26:00 -0500

    another attemp 😡

---
+++ Assets/Scripts/ManagerAndControllers/Controllers/TitleController.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+//using static UnityEditor.Progress;
+
+public class TitleController : MonoBehaviour
+{
+    [Header("Buttons")]
+    public GameObject ButtonPanel;
+    public Button PlayButton;
+    public Button ResumeButton;
+    public Button OptionsButton;
+    public Button QuitButton;
+
+    [Header("Game Status Info")]
+    public TextMeshProUGUI VersionText;
+
+    private GameData latestSave = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VersionText.SetText("Version: " + Application.version);
+
+        // Add listeners to buttons
+        PlayButton.onClick.AddListener(StartGame);
+        ResumeButton.onClick.AddListener(ResumeGame);
+        OptionsButton.onClick.AddListener(OpenOptions);
+        QuitButton.onClick.AddListener(Quit);
+
+        CheckForSaveData();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Plays Sound for when mouse over Button.
+    /// 0 references because from inspector.
+    /// </summary>
+    public void PlayButtonSound()
+    {
+        SoundManager.PlayFXSound(SoundFX.MenuSelectionSound);
+    }
+
+    /// <summary>
+    /// Start a new game.
+    /// Set Default Player Stats
+    /// </summary>
+   private void StartGame()
+   {
+        GameData startData = new GameData();
+
+        startData.SaveName = "Beginning";
+        startData.Level = Levels.Tutorial;
+        startData.MaxHealth = 50;
+        startData.Health = 50;
+        startData.Scraps = 100;
+        startData.TimeStamp = DateTime.Now;
+
+
+        // Adds gear to list.
+        
+        foreach (Item gear in GearManager.Instance.StartingGear)
+        {
+            Item gearInstance = Instantiate(gear);
+            GearManager.Instance.Acquire(gearInstance);
+            GearManager.Instance.EquipGear(gearInstance); 
+        }
+
+        // Add Starting Chips
+        foreach(NewChip newChip in ChipManager.Instance.StartingChips)
+        {
+            NewChip chipInstance = Instantiate(newChip);
+
+            ChipManager.Instance.AddNewChipToDeck(chipInstance);
+        }
+        
+        DataManager.Instance.CurrentGameData=startData;
+
+        DataManager.Instance.Save(startData.SaveName);
+
+        GameManager.Instance.RequestScene(Levels.Tutorial);
+
+    }
+    /// <summary>
+    /// Logic for resuming the game.
+    /// </summary>
+    private void ResumeGame()
+    {
+        // Load the save data into the game
+        DataManager.Instance.LoadData(latestSave.SaveName);
+
+        // Request the scene from GameManager
+        GameManager.Instance.RequestScene(latestSave.Level);
+    }
+    private void OpenOptions()
+    {
+
+    }
+    /// <summary>
+    /// Quit Game
+    /// </summary>
+    private void Quit()
+    {
+        Application.Quit();
+    }
+    /// <summary>
+    /// Get all saves from DataManager
+    /// Enable or disable the ResumeButton based on save availability
+    /// </summary>
+    private void CheckForSaveData()
+    {    
+        List<GameData> allSaves = DataManager.Instance.GetAllSaves();
+
+        if (allSaves != null && allSaves.Count > 0)
+            ResumeButton.interactable = true;        
+        else
+            ResumeButton.interactable = false;
+
+        // Find the latest save by timestamp        
+        foreach (var save in allSaves)
+        {
+            if (latestSave == null || save.TimeStamp > latestSave.TimeStamp)
+                latestSave = save;
+        }
+    }
+    [ContextMenu("Bring in Buttons")]
+    private void BringInButtons()
+    {
+        ButtonPanel.GetComponent<Animator>().SetTrigger("BringInButtons");
+    }
+}
