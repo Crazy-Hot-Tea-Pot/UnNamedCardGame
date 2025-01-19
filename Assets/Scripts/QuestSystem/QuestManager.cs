6318f1f8c73e1fa9c3b5131005290c80cc00dcfe
Sebastian <sebander01@gmail.com>
2025-01-19 02:18:46 -0500

    Quest System Progress
    Quest Manager is complete I got held up on some issues but am past them now. UI needs to be created, Backend for one quest complete. Need to add visual aspects to the system. I got a little held up on some issues I was having that I have now fixed.

---
+++ Assets/Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestManager : MonoBehaviour
+{
+    public static QuestManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+
+    private static QuestManager instance;
+
+    [Tooltip("A list of all quests availiable in order")]
+    public List<Quest> questList;
+    public List<Quest> completeList;
+
+    private TMP_Text textSpeaker;
+    private TMP_Text textPlayer;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //We don't want to effect the originals so we are instantly copying the quest into its self so it's a list of copiess
+        for (int i = 0; i < questList.Count; i++)
+        {
+
+            //Clone the value
+            Quest Temp = questList[i];
+            questList[i] = Instantiate(Temp);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This line tells the manager to stop if we run out of quests
+        if(questList.Count != 0)
+        {
+            //If the current quest is complete
+            if (questList[0].complete == true)
+            {
+                //Add to the complete list
+                completeList.Add(questList[0]);
+                //Remove the quest from the list
+                questList.RemoveAt(0);
+            }
+            //If the quest is not complete then all the quests action
+            else if (questList[0].complete == false)
+            {
+                questList[0].speaking(textSpeaker, textPlayer);
+            }
+        }
+    }
+}
